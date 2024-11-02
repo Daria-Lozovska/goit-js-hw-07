e2df50df11283ad7b96bd13f61a4e3f1a34f61b5
lozovskaadasa9@gmail.com <lozovskaadasa9@gmail.com>
2024-11-02 20:21:57 +0100

    add files with all done but 6 task is not for all

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,44 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector("#controls input");
+const createButton = document.querySelector("[data-create]");
+const destroyButton = document.querySelector("[data-destroy]");
+const boxesContainer = document.querySelector("#boxes");
+
+function createBoxes(amount) {
+  boxesContainer.innerHTML = "";
+
+  const boxes = [];
+  let size = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.push(box);
+    size += 10;
+  }
+
+  boxesContainer.append(...boxes);
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = "";
+}
+
+
+createButton.addEventListener("click", () => {
+  const amount = Number(input.value);
+
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    input.value = ""; 
+  } else {
+    alert("Please enter a number between 1 and 100.");
+  }
+});
+
+destroyButton.addEventListener("click", destroyBoxes);
